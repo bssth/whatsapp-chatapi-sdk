17d775a871eb9c395b8cd1b66460f8a6a2e0ce3d
Mike Chip <mikael.chip@gmail.com>
2018-07-26 17:04:03 +0300

    Update chatapi.class.php

--- chatapi.class.php
+++ chatapi.class.php
@@ -9,34 +9,34 @@
 		protected $url = '';
 
 		/**
-         * Create instance
-         * @param string $token
-         * @param string $url
-         * @return void
-         */
+		 * Create instance
+		 * @param string $token
+		 * @param string $url
+		 * @return void
+		 */
 		public function __construct($token, $url = 'https://foo.chat-api.com')
 		{
 			$this->token = $token;
 			$this->url = $url;
 		}
 
-        /**
-         * Generate query URL
-         * @param string $method
-         * @return string
-         */
+		/**
+		 * Generate query URL
+		 * @param string $method
+		 * @return string
+		 */
 		public function createUrl($method)
-        {
-            return $this->url.'/'.$method.'?token=' . $this->token;
-        }
+        	{
+            		return $this->url.'/'.$method.'?token=' . $this->token;
+        	}
 
-        /**
-         * Send chat-api query
-         * @param string $method
-         * @param null|array $args
-         * @param string $qmethod
-         * @return bool|string
-         */
+		/**
+		 * Send chat-api query
+		 * @param string $method
+		 * @param null|array $args
+		 * @param string $qmethod
+		 * @return bool|string
+		 */
 		public function query($method, $args = null, $qmethod = 'POST')
 		{
 			$url = $this->createUrl($method);
@@ -55,7 +55,7 @@
 		}
 
 		/**
-         * Get all inbox messages (doesn't include queue)
+         	 * Get all inbox messages (doesn't include queue)
 		 * @return null|array
 		 */
 		public function getInbox()
@@ -63,25 +63,25 @@
 			return array_reverse(json_decode($this->query('messages'), 1)['messages']);
 		}
 
-        /**
-         * Get status (logged in / error / loading) for current instance
-         * @return string
-         */
-        public function getStatus()
-        {
-            $js = json_decode($this->query('status'), 1);
+		/**
+		 * Get status (logged in / error / loading) for current instance
+		 * @return string
+		 */
+		public function getStatus()
+		{
+		    $js = json_decode($this->query('status'), 1);
 
-            if(isset($js['accountStatus']))
-                return $js['accountStatus'];
-            else
-                return 'error';
-        }
+		    if(isset($js['accountStatus']))
+			return $js['accountStatus'];
+		    else
+			return 'error';
+		}
 
-        /**
-         * Get all messages from chat by ID
-         * @param string $author
-         * @return array
-         */
+		/**
+		 * Get all messages from chat by ID
+		 * @param string $author
+		 * @return array
+		 */
 		public function getChatMessages($author)
 		{
 			$ib = $this->getInbox();
@@ -95,59 +95,59 @@
 			return $msgs;
 		}
 
-        /**
-         * Send message to phone number
-         * @param string $chat
-         * @param string $text
-         * @return boolean
-         */
+		/**
+		 * Send message to phone number
+		 * @param string $chat
+		 * @param string $text
+		 * @return boolean
+		 */
 		public function sendPhoneMessage($chat, $text)
 		{
 			return json_decode($this->query('sendMessage', ['phone' => $chat, 'body' => $text]), 1)['sent'];
 		}
 
-        /**
-         * Log out current instance
-         * @return null|array
-         */
-        public function logout()
-        {
-            return json_decode($this->query('logout', []));
-        }
+		/**
+		 * Log out current instance
+		 * @return null|array
+		 */
+		public function logout()
+		{
+		    return json_decode($this->query('logout', []));
+		}
 
-        /**
-         * Send reboot signal to instance
-         * @return null|array
-         */
-        public function reboot()
-        {
-            return json_decode($this->query('reboot', []));
-        }
+		/**
+		 * Send reboot signal to instance
+		 * @return null|array
+		 */
+		public function reboot()
+		{
+		    return json_decode($this->query('reboot', []));
+		}
 
-        /**
-         * Generate QR-code direct link
-         * @return string
-         */
-        public function getQRCode()
-        {
-            return $this->createUrl('qr_code');
-        }
+		/**
+		 * Generate QR-code direct link
+		 * @return string
+		 */
+		public function getQRCode()
+		{
+		    return $this->createUrl('qr_code');
+		}
 
-        /**
-         * Send message to chat (by not phone but chat ID)
-         * @param string $chat
-         * @param string $text
-         * @return boolean
-         */
+		/**
+		 * Send message to chat (by not phone but chat ID)
+		 * @param string $chat
+		 * @param string $text
+		 * @return boolean
+		 */
 		public function sendMessage($chat, $text)
 		{
 			return json_decode($this->query('sendMessage', ['chatId' => $chat, 'body' => $text]), 1)['sent'];
 		}
 
-        /**
-         * Generate dialogues list from messages
-         * @return array
-         */
+		/**
+		 * Generate dialogues list from messages
+		 * @return array
+		 */
 		public function getDialogs()
 		{
 			$ib = $this->getInbox();
